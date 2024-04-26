6ace7a4ae0f44acd4991adbd29f44333a11a8635
Morgan Asare <ohenemorgan@gmail.com>
2024-04-26 09:31:16 +0000

    Add dunder method to Comment model

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    field_2 = models.CharField(default='Hello world')
+    field_3 = models.CharField(null=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -33,3 +35,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body}. by {self.author}"
